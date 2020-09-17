ed54949bfff09a2485597e40d78036d55a23473d
kellychihjau <kelly@Kellys-MacBook-Air.local>
2020-09-17 21:10:49 +0700

    test branch kelly

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
         </div>
     </div>
     <div class="col-md-6 login-panel right">
-        <h3 class="font-normal text-highlight">เข้าสู่ระบบ</h3>
+        <h3 class="font-normal text-highlight">เข้าสู่ระบบ Branch Kelly</h3>
         <form method="POST" action="{{ route('login') }}">
             @csrf
 <!--             <div class="form-group">
