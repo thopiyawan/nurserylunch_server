9bbaef5703e34777a41ceed86c615bc0035838b5
kellychihjau <kelly@Kellys-MacBook-Air.local>
2020-10-29 10:44:10 +0700

    .

--- resources/views/mealplan/mealdate.blade.php
+++ resources/views/mealplan/mealdate.blade.php
@@ -7,7 +7,32 @@
     @if ($userSetting->is_breakfast == 1)
         <div class="col col-meal" id="breakfast-meal-{{ $day }}" date-date="123">
             <div class="mlabel">เช้า</div>
-            <div id="" class="ui-sortable ui-sortable-meal">
+             <div id="" class="ui-sortable ui-sortable-meal breakfast-snack-{{$day}} {{$setting_id}}" data-day="{{$day}}" data-meal="breakfast-snack" data-type="{{$setting_id}}">
+                @foreach ($food_logs as $food)
+                    @if ($food->meal_code == 1 && $food->meal_date == $date_in_week)
+                        <div class="ui-sortable ui-sortable-meal">
+                            <div class="menu-body">
+                                <div class="col col-food-name" data-energy="{{ $food->energy }}"
+                                    data-protein="{{ $food->protein }}" data-fat="{{ $food->fat }}"
+                                    id="{{ $food->food_id }}">
+                                    {{ $food->food_thai }}
+                                </div>
+                                <div class="col col-delete">
+                                    <a class="pull-right">
+                                        <span><i class="fas fa-times"></i></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+                <div class="ui-sortable">
+                    <div class="text-center menu-body ui-sortable-handle ui-sortable-placeholder ui-state-disabled">
+                        <span class=""><i class="fa fa-hand-pointer-o"></i>วางที่นี่</span>
+                    </div>
+                </div>
+            </div>
+            <!-- <div id="" class="ui-sortable ui-sortable-meal">
                 @foreach ($food_logs as $food)
                     @if ($food->meal_code == 1 && $food->meal_date == $date_in_week)
                         <div class="ui-sortable ui-sortable-meal">
@@ -33,7 +58,7 @@
                 </div>
                 <button type="button" class="btn btn-sm btn-default" data-toggle="tooltip" data-placement="right"
                     title="" data-original-title="Tooltip on left">Hello</button>
-            </div>
+            </div> -->
         </div>
     @endif
 
@@ -72,7 +97,7 @@
     @if ($userSetting->is_lunch == 1)
         <div class="col col-meal" id="lunch-meal-{{ $day }}">
             <div class="mlabel">กลางวัน</div>
-            <div id="" class="ui-sortable ui-sortable-meal">
+            <div id="" class="ui-sortable ui-sortable-meal lunch-{{$day}} {{$setting_id}}" data-day="{{$day}}" data-meal="lunch" data-type="{{$setting_id}}">
                 @foreach ($food_logs as $food)
                     @if ($food->meal_code == 3 && $food->meal_date == $date_in_week)
                         <div class="ui-sortable ui-sortable-meal">
@@ -97,13 +122,38 @@
                     </div>
                 </div>
             </div>
+            <!-- <div id="" class="ui-sortable ui-sortable-meal">
+                @foreach ($food_logs as $food)
+                    @if ($food->meal_code == 3 && $food->meal_date == $date_in_week)
+                        <div class="ui-sortable ui-sortable-meal">
+                            <div class="menu-body">
+                                <div class="col col-food-name" data-energy="{{ $food->energy }}"
+                                    data-protein="{{ $food->protein }}" data-fat="{{ $food->fat }}"
+                                    id="{{ $food->food_id }}">
+                                    {{ $food->food_thai }}
+                                </div>
+                                <div class="col col-delete">
+                                    <a class="pull-right">
+                                        <span><i class="fas fa-times"></i></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+                <div class="ui-sortable">
+                    <div class="text-center menu-body ui-sortable-handle ui-sortable-placeholder ui-state-disabled">
+                        <span class=""><i class="fa fa-hand-pointer-o"></i>วางที่นี่</span>
+                    </div>
+                </div>
+            </div> -->
         </div>
     @endif
 
     @if ($userSetting->is_afternoon_snack)
         <div class="col col-meal" id="lunch-snack-meal-{{ $day }}">
             <div class="mlabel">ว่างบ่าย</div>
-            <div id="" class="ui-sortable ui-sortable-meal">
+            <div id="" class="ui-sortable ui-sortable-meal lunch-snack-{{$day}} {{$setting_id}}" data-day="{{$day}}" data-meal="lunch-snack" data-type="{{$setting_id}}">
                 @foreach ($food_logs as $food)
                     @if ($food->meal_code == 4 && $food->meal_date == $date_in_week)
                         <div class="ui-sortable ui-sortable-meal">
@@ -128,6 +178,31 @@
                     </div>
                 </div>
             </div>
+            <!-- <div id="" class="ui-sortable ui-sortable-meal">
+                @foreach ($food_logs as $food)
+                    @if ($food->meal_code == 4 && $food->meal_date == $date_in_week)
+                        <div class="ui-sortable ui-sortable-meal">
+                            <div class="menu-body">
+                                <div class="col col-food-name" data-energy="{{ $food->energy }}"
+                                    data-protein="{{ $food->protein }}" data-fat="{{ $food->fat }}"
+                                    id="{{ $food->food_id }}">
+                                    {{ $food->food_thai }}
+                                </div>
+                                <div class="col col-delete">
+                                    <a class="pull-right">
+                                        <span><i class="fas fa-times"></i></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+                <div class="ui-sortable">
+                    <div class="text-center menu-body ui-sortable-handle ui-sortable-placeholder ui-state-disabled">
+                        <span class=""><i class="fa fa-hand-pointer-o"></i>วางที่นี่</span>
+                    </div>
+                </div>
+            </div> -->
         </div>
     @endif
 
